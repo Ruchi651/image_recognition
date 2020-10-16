834389107cd41ad459292648b677c6aca9943960
Ruchi Shukla <sruchi0605@gmail.com>
2020-10-16 17:01:46 +0530

    Bug fixed! Resizing completed.

---
+++ resize_images.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import pathlib
+from pathlib import Path
+maxsize = (512, 512)
+for input_img_path in pathlib.Path("input").iterdir():
+    output_img_path = str(input_img_path).replace("input","output")
+    with Image.open(input_img_path) as im:
+        im.thumbnail(maxsize)
+        im.save(output_img_path, "JPEG", dpi=(300,300))
+
+
